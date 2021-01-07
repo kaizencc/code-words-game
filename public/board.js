aef4e8eb98bf10dfe7394a6aa13b43d61be842ac
Kaizen Conroy <kcc2@williams.edu>
2021-01-06 19:36:01 -0500

    add radio buttons for spymaster/field operative

--- public/board.js
+++ public/board.js
@@ -1,5 +1,7 @@
 const board = document.getElementById('board');
 const newGameBtn = document.getElementById('newgame');
+const fieldOperator = document.getElementById('field');
+const spyMaster = document.getElementById('spy');
 
 // Build board buttons when a new user joins the room.
 socket.on('board-game', (data) => {
@@ -15,7 +17,7 @@ socket.on('board-game', (data) => {
 function createButton(word){
     var btn = document.createElement("button");
     btn.style.width = "18%";
-    btn.style.height= "20%";
+    btn.style.height= "25%";
     btn.id = word[0];
     btn.className = "m-1 p-auto btn";
     btn.classList.add(word[1])
@@ -29,10 +31,21 @@ board.addEventListener('click', function(e){
     const text = e.target.id;
     console.log(text);
     output.innerHTML += '<p>--> <strong><em>' + username + ' </strong>clicked ' + text + '</em></p>';
+    document.getElementById('chat-message').scrollTop = document.getElementById('chat-message').scrollHeight
 })
 
 // Listening for new game request.
 newGameBtn.addEventListener('click', () =>{
     socket.emit('new-game',{username: username, roomname: roomname});
+})
+
+spyMaster.addEventListener('click', ()=>{
+    spyMaster.className = "btn btn-outline-primary active"
+    fieldOperator.className = "btn btn-outline-primary"
+})
+
+fieldOperator.addEventListener('click', () =>{
+    spyMaster.className = "btn btn-outline-primary"
+    fieldOperator.className = "btn btn-outline-primary actve"
 })
 
