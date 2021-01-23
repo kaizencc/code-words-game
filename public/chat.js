0d802a382e99af11f73bb18664d9092f4b461090
Kaizen Conroy <kcc2@williams.edu>
2021-01-23 13:12:37 -0500

    update ip address

--- public/chat.js
+++ public/chat.js
@@ -8,8 +8,8 @@ const blueUsers = document.getElementById('users-blue');
 
 //Socket server URL
 const localIp = "localhost";
-const awsIp = "3.87.185.72";
-const socket = io.connect(`http://${localIp}:3000`);
+const awsIp = "34.205.165.2";
+const socket = io.connect(`http://${awsIp}:3000`);
 
 //Fetch URL Params from URL
 const queryString = window.location.search;
