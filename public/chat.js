c30930605c02ad34fe7a839dd4b804adedd64bbc
kaizen3031593 <36202692+kaizen3031593@users.noreply.github.com>
2021-01-24 20:28:21 -0500

    Update README.md
    update socket.io
    
    back to elastic ip
    
    back
    
    works

--- public/chat.js
+++ public/chat.js
@@ -9,7 +9,7 @@ const blueUsers = document.getElementById('users-blue');
 //Socket server URL
 const hostname = "localhost";
 const elasticIp = "35.172.99.231";
-const socket = io.connect(`http://${hostname}:3000`);
+const socket = io.connect();
 
 //Fetch URL Params from URL
 const queryString = window.location.search;
