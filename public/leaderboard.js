01b4d8493a18dcd6dc2384e771a14f6ace75870e
Kaizen Conroy <kcc2@williams.edu>
2021-02-01 23:46:21 -0500

    colors and ordering

---
+++ public/leaderboard.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let elements = [];
+    let leaderboard = document.getElementById('leaderboard');
+    // Add each row to the array
+    leaderboard.querySelectorAll('.item').forEach(el => elements.push(el));
+    // Clear the container
+    leaderboard.innerHTML = '';
+    // Sort the array from highest to lowest
+    elements.sort((a, b) => b.querySelector('.clues').textContent - a.querySelector('.clues').textContent);
+    // Put the elements back into the container
+    console.log(elements);
+    elements.forEach(e => leaderboard.appendChild(e));
+  })
