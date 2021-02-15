7e0c9f9d185fda81e1bb3dd0800ec76e783b962f
Kaizen Conroy <kcc2@williams.edu>
2021-02-15 18:26:25 -0500

    lobby works for one screen

--- public/lobby.js
+++ public/lobby.js
@@ -1,19 +1,23 @@
+const lobbyBtn = document.getElementById('lobbybtn');
+const usernameSpace = document.getElementById('username');
 const lobby = document.getElementById('playerlobby');
 
-createPlayerAvatar("Bobby");
-createPlayerAvatar("Bobby");
-createPlayerAvatar("Bobby");
-createPlayerAvatar("Bobby");
-createPlayerAvatar("Bobby");
-createPlayerAvatar("Bobby");
-createPlayerAvatar("Bobby");
+/**
+ * When enter is clicked, validate the username and create an avatar in the player lobby.
+ */
+lobbyBtn.addEventListener('click', () =>{
+    createPlayerAvatar(usernameSpace.value);
+})
 
-
+/**
+ * Creates the html required for the player avatar to show up on screen.
+ * 
+ * @param {string} username The players username
+ */
 function createPlayerAvatar(username){
     const player = document.createElement('div');
     player.className = 'col';
     player.style = "max-width: 25%";
-
 
     const avatar = document.createElement('div');
     avatar.className = "avatar text-center";
